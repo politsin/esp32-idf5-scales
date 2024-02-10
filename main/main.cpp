42ee99c36c6c535879cbd2eb24de5cf27ad97386
Anatoly Politsin <politsin@gmail.com>
2024-02-11 00:49:24 +0300

    uart

--- main/main.cpp
+++ main/main.cpp
@@ -28,11 +28,11 @@ extern "C" void app_main(void) {
 
   // tasks.
   xTaskCreate(&loop, "loop", min * 3, NULL, 2, NULL);
-  xTaskCreate(uartTask, "uart", min * 2, NULL, 1, &uart);
+  xTaskCreate(uartTask, "uart", min * 10, NULL, 1, &uart);
   xTaskCreate(hx711Task, "hx711", min * 16, NULL, 1, &hx711);
   xTaskCreate(blinkTask, "blink", min * 2, NULL, 1, &blink);
-  xTaskCreate(buttonTask, "button", min * 3, NULL, 1, &button);
-  xTaskCreate(encoderTask, "encoder", min * 3, NULL, 1, &encoder);
+  xTaskCreate(buttonTask, "button", min * 4, NULL, 1, &button);
+  xTaskCreate(encoderTask, "encoder", min * 6, NULL, 1, &encoder);
   // xTaskCreate(screenTask, "screen", min * 64, NULL, 1, &screen);
 }
 
--- main/task/uartTask.cpp
+++ main/task/uartTask.cpp
@@ -3,20 +3,56 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include "driver/gpio.h"
+#include "driver/uart.h"
 #include <esp_log.h>
 #include <hal/gpio_types.h>
+#include <hal/uart_types.h>
+#include <rom/gpio.h>
+#include <stdio.h>
+#include <esp_err.h>
+
 #define UART_TAG "UART"
+#define BUF_SIZE (1024)
 
 typedef gpio_num_t Pintype;
-static constexpr Pintype EN = GPIO_NUM_2;
-static constexpr Pintype DIR = GPIO_NUM_15;
-static constexpr Pintype STEP = GPIO_NUM_13;
-
+static constexpr Pintype RTS = GPIO_NUM_2;
+static constexpr Pintype TX = GPIO_NUM_15;
+static constexpr Pintype RX = GPIO_NUM_13;
+// GND ↑ GND--13--15--2
+static constexpr Pintype CTS = GPIO_NUM_19; 
+static int badu = 115200;
+uart_port_t uart_no = 2;
 
 TaskHandle_t uart;
 void uartTask(void *pvParam) {
   const TickType_t xBlockTime = pdMS_TO_TICKS(3 * 1000);
+  uart_config_t uart_config = {
+      .baud_rate = badu,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+      .source_clk = UART_SCLK_DEFAULT,
+  };
+  
+  int intr_alloc_flags = 0;
+  ESP_ERROR_CHECK(
+      uart_driver_install(uart_no, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+  ESP_ERROR_CHECK(uart_param_config(uart_no, &uart_config));
+  ESP_ERROR_CHECK(uart_set_pin(uart_no, TX, RX, RTS, CTS));
+  uint8_t *data = (uint8_t *)malloc(BUF_SIZE);
+
   while (true) {
+    // Read data from the UART
+    int len =
+        uart_read_bytes(uart_no, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+    // Write data back to the UART
+    uart_write_bytes(uart_no, (const char *)data, len);
+    if (len) {
+      data[len] = '\0';
+      ESP_LOGI(UART_TAG, "Recv str: %s", (char *)data);
+    }
     if (true) {
       ESP_LOGW(UART_TAG, "uart!");
     }
